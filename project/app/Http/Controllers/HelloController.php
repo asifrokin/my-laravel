125ee768505675a276be5892dcc597f4e4eb5bd5
Rokin <rokinkst@gmail.com>
2018-01-10 11:09:41 +0600

    Laravel Installed

---
+++ project/app/Http/Controllers/HelloController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+class HelloController extends Controller
+{
+    public function hello(){
+        $hello = 1+4;
+        ;
+    }
+
+}
